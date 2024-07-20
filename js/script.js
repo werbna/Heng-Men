fd96f6e3f17bb1e0df7d3781a399b6627cf508ad
werbnah <andrewchau1993@gmail.com>
2024-07-20 15:35:45 -0700

    html and css grabbed from another source and added varaibles

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector("button");
+const maxGuesses = 6;
+
+let currentWord, correctLetters, wrongGuessCount;
+
